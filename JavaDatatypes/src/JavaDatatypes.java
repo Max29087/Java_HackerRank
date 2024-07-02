cb4e8b9fd263302d54e145d02feb1402b3a2d329
Max29087 <topusorker087@gmai.com>
2024-07-02 20:04:11 +0600

    Add new file

--- JavaDatatypes/src/JavaDatatypes.java
+++ JavaDatatypes/src/JavaDatatypes.java
@@ -1,40 +1,29 @@
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class JavaDatatypes {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        BigInteger[] arr = new BigInteger[n];
+        Scanner in = new Scanner(System.in);
+        int n;
+        long x;
+        n = in.nextInt();
         for (int i = 0; i < n; i++) {
-            arr[i] = input.nextBigInteger();
-        }
-        BigInteger byteMin = BigInteger.valueOf(Byte.MIN_VALUE);
-        BigInteger byteMax = BigInteger.valueOf(Byte.MAX_VALUE);
-        BigInteger shortMin = BigInteger.valueOf(Short.MIN_VALUE);
-        BigInteger shortMax = BigInteger.valueOf(Short.MAX_VALUE);
-        BigInteger intMin = BigInteger.valueOf(Integer.MIN_VALUE);
-        BigInteger intMax = BigInteger.valueOf(Integer.MAX_VALUE);
-        BigInteger longMin = BigInteger.valueOf(Long.MIN_VALUE);
-        BigInteger longMax = BigInteger.valueOf(Long.MAX_VALUE);
-
-        for (int i = 0; i < n; i++) {
-            if (arr[i].compareTo(longMax) > 0) {
-                System.out.println(arr[i] + " can't be fitted anywhere.");
-            } else {
-                System.out.println(arr[i] + " can be fitted in:");
-                if (arr[i].compareTo(byteMin) >= 0 && arr[i].compareTo(byteMax) <= 0) {
+            try {
+                x = in.nextLong();
+                System.out.println(x + " can be fitted in:");
+                if (x <= Byte.MAX_VALUE && x >= Byte.MIN_VALUE) {
                     System.out.println("* byte");
                 }
-                if (arr[i].compareTo(shortMin) >= 0 && arr[i].compareTo(shortMax) <= 0) {
+                if (x <= Short.MAX_VALUE && x >= Short.MIN_VALUE) {
                     System.out.println("* short");
                 }
-                if (arr[i].compareTo(intMin) >= 0 && arr[i].compareTo(intMax) <= 0) {
+                if (x <= Integer.MAX_VALUE && x >= Integer.MIN_VALUE) {
                     System.out.println("* int");
                 }
-                if (arr[i].compareTo(longMin) >= 0 && arr[i].compareTo(longMax) <= 0) {
+                if (x <= Long.MAX_VALUE) {
                     System.out.println("* long");
                 }
+            } catch (Exception e) {
+                System.out.println(in.next()+" can't be fitted anywhere.");
             }
 
         }
